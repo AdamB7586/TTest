15816ecb2aea4d90ffc8d3f253fe6d02cfb3c235
Adam Binnersley <abinnersley@gmail.com>
2017-09-05 16:14:01 +0100

    Update comments & bug fixes

--- src/LearnTest.php
+++ src/LearnTest.php
@@ -2,6 +2,8 @@
 namespace TheoryTest\Car;
 
 use DBAL\Database;
+use Smarty;
+use UserAuth\User;
 use HighwayCode;
 
 class LearnTest extends TheoryTest{
@@ -19,14 +21,14 @@ class LearnTest extends TheoryTest{
 
 
     /**
-     * Connects to the database sets the current user and gets any user answers
-     * @param Database $db
-     * @param Smarty $layout
-     * @param User $user
-     * @param int|false $userID
+     * Set up all of the components needed to create a Theory Test
+     * @param Database $db This should be an instance of Database
+     * @param Smarty $layout This needs to be an instance of Smarty Templating
+     * @param User $user This should be and instance if the User Class
+     * @param false|int $userID If you wish to emulate a user set this value to the users ID else set to false
      */
     public function __construct(Database $db, Smarty $layout, User $user, $userID = false) {
-        parent::__construct($db, $layout, $user);
+        parent::__construct($db, $layout, $user, $userID);
         $this->getTestInfo();
     }
     

ce9fe0a27a34e33c1ebce40a3c697afc18d19e1d
Adam Binnersley <abinnersley@gmail.com>
2017-09-06 13:20:23 +0100

    Additional class testing

---
+++ tests/LearnTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace TheoryTest\Tests;
+
+use DBAL\Database;
+use Smarty;
+use UserAuth\User;
+use TheoryTest\Car\LearnTest;
+use PHPUnit\Framework\TestCase;
+
+class LearnTest extends TestCase{
+    
+    protected static $db;
+    protected static $user;
+    protected static $template;
+    protected static $learnTest;
+    
+    public function setUp() {
+        self::$db = new Database($GLOBALS['DB_HOST'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'], $GLOBALS['DB_DBNAME']);
+        self::$template = new Smarty();
+        self::$user = new User(self::$db);
+        self::$learnTest = new LearnTest(self::$db, self::$template, self::$user);
+    }
+    
+    public function tearDown() {
+        unset(self::$db);
+        unset(self::$template);
+        unset(self::$user);
+        unset(self::$learnTest);
+    }
+    
+    public function testConnection() {
+        $this->markTestIncomplete();
+    }
+}
---
+++ tests/RandomTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace TheoryTest\Tests;
+
+use DBAL\Database;
+use Smarty;
+use UserAuth\User;
+use TheoryTest\Car\RandomTest;
+use PHPUnit\Framework\TestCase;
+
+class RandomTest extends TestCase{
+    
+    protected static $db;
+    protected static $user;
+    protected static $template;
+    protected static $randomTest;
+    
+    public function setUp() {
+        self::$db = new Database($GLOBALS['DB_HOST'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'], $GLOBALS['DB_DBNAME']);
+        self::$template = new Smarty();
+        self::$user = new User(self::$db);
+        self::$randomTest = new RandomTest(self::$db, self::$template, self::$user);
+    }
+    
+    public function tearDown() {
+        unset(self::$db);
+        unset(self::$template);
+        unset(self::$user);
+        unset(self::$randomTest);
+    }
+    
+    public function testConnection() {
+        $this->markTestIncomplete();
+    }
+}

bfcf8bc095d0c98ae70e9e07795e1cd8f9997037
Adam Binnersley <abinnersley@gmail.com>
2017-09-13 16:54:14 +0100

    Call static db method

--- src/RandomTest.php
+++ src/RandomTest.php
@@ -9,18 +9,6 @@ class RandomTest extends TheoryTest{
     protected $testNo = 15;
     
     protected $scriptVar = 'random';
-
-
-    /**
-     * Set up all of the components needed to create a Theory Test
-     * @param Database $db This should be an instance of Database
-     * @param Smarty $layout This needs to be an instance of Smarty Templating
-     * @param object $user This should be and instance if the User Class
-     * @param false|int $userID If you wish to emulate a user set this value to the users ID else set to false
-     */
-    public function __construct(Database $db, Smarty $layout, $user, $userID = false) {
-        parent::__construct($db, $layout, $user, $userID);
-    }
     
     /**
      * Create a new Random Theory Test for the test number given
@@ -43,8 +31,8 @@ class RandomTest extends TheoryTest{
      * @return boolean If the questions are inserted into the database will return true else returns false
      */
     protected function chooseQuestions($testNo) {
-        $this->db->delete($this->progressTable, array('user_id' => self::$user->getUserID(), 'test_id' => $testNo));
-        $questions = $this->db->query("SELECT * FROM ((SELECT `prim` FROM `".$this->questionsTable."` WHERE `dsacat` = '1' AND `carquestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 2)
+        self::$db->delete($this->progressTable, array('user_id' => self::$user->getUserID(), 'test_id' => $testNo));
+        $questions = self::$db->query("SELECT * FROM ((SELECT `prim` FROM `".$this->questionsTable."` WHERE `dsacat` = '1' AND `carquestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 2)
 UNION (SELECT `prim` FROM `".$this->questionsTable."` WHERE `dsacat` = '2' AND `carquestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 3)
 UNION (SELECT `prim` FROM `".$this->questionsTable."` WHERE `dsacat` = '3' AND `carquestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 4)
 UNION (SELECT `prim` FROM `".$this->questionsTable."` WHERE `dsacat` = '4' AND `carquestion` = 'Y' AND `alertcasestudy` IS NULL LIMIT 3)
@@ -65,6 +53,6 @@ UNION (SELECT `prim` FROM `".$this->questionsTable."` WHERE `casestudyno` = '".r
             $this->questions[$q] = $question['prim'];
             $q++;
         }
-        return $this->db->insert($this->progressTable, array('user_id' => self::$user->getUserID(), 'questions' => serialize($this->questions), 'answers' => serialize(array()), 'test_id' => $testNo, 'started' => date('Y-m-d H:i:s'), 'status' => 0, 'type' => strtolower($this->getTestType())));
+        return self::$db->insert($this->progressTable, array('user_id' => self::$user->getUserID(), 'questions' => serialize($this->questions), 'answers' => serialize(array()), 'test_id' => $testNo, 'started' => date('Y-m-d H:i:s'), 'status' => 0, 'type' => strtolower($this->getTestType())));
     }
 }

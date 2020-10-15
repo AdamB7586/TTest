e681c8eaaa5c9eb9b73dc52d13565f59eade5169
Adam Binnersley <abinnersley@gmail.com>
2020-10-15 13:58:56 +0100

    Update unit tests

--- tests/LearnTestTest.php
+++ tests/LearnTestTest.php
@@ -15,8 +15,91 @@ class LearnTestTest extends SetUp
         $this->learnTest = new LearnTest($this->db, $this->config, $this->template, $this->user);
     }
     
-    public function testConnection()
+    /**
+     * @covers TheoryTest\Car\LearnTest::__construct
+     * @covers TheoryTest\Car\LearnTest::setTables
+     * @covers TheoryTest\Car\LearnTest::getSectionInfo
+     * @covers TheoryTest\Car\LearnTest::getTestInfo
+     * @covers TheoryTest\Car\LearnTest::chooseStudyQuestions
+     * @covers TheoryTest\Car\LearnTest::createNewTest
+     * @covers TheoryTest\Car\LearnTest::currentQuestion
+     * @covers TheoryTest\Car\LearnTest::getFirstQuestion
+     * @covers TheoryTest\Car\LearnTest::getQuestions
+     * @covers TheoryTest\Car\LearnTest::getTestName
+     * @covers TheoryTest\Car\LearnTest::getUserAnswers
+     * @covers TheoryTest\Car\LearnTest::numQuestions
+     * @covers TheoryTest\Car\LearnTest::questionNo
+     * @covers TheoryTest\Car\LearnTest::updateTestProgress
+     * @covers TheoryTest\Car\TheoryTest::__construct
+     * @covers TheoryTest\Car\TheoryTest::setTables
+     * @covers TheoryTest\Car\TheoryTest::clearSettings
+     * @covers TheoryTest\Car\TheoryTest::setTest
+     * @covers TheoryTest\Car\TheoryTest::setTestName
+     * @covers TheoryTest\Car\TheoryTest::buildTest
+     * @covers TheoryTest\Car\TheoryTest::checkSettings
+     * @covers TheoryTest\Car\TheoryTest::createQuestionHTML
+     * @covers TheoryTest\Car\TheoryTest::getJavascriptLocation
+     * @covers TheoryTest\Car\TheoryTest::getQuestionData
+     * @covers TheoryTest\Car\TheoryTest::getTest
+     * @covers TheoryTest\Car\TheoryTest::getTestType
+     * @covers TheoryTest\Car\TheoryTest::getUserID
+     * @covers TheoryTest\Car\TheoryTest::setImagePath
+     * @covers TheoryTest\Car\TheoryTest::setImageRootPath
+     * @covers TheoryTest\Car\User::checkUserAccess
+     * @covers TheoryTest\Car\User::getUserSettings
+     * @covers TheoryTest\Car\User::setUserSettings
+     */
+    public function testCreateNewTest()
     {
+        $learnTest = $this->learnTest->createNewTest();
+        $this->assertStringStartsWith('<div class="row">', $learnTest);
+        //$this->markTestIncomplete();
+    }
+    
+    /**
+     * @covers TheoryTest\Car\TheoryTest::createQuestionHTML
+     */
+    public function testCreateQuestionHTML()
+    {
+        $this->markTestIncomplete();
+    }
+    
+    /**
+     * @covers TheoryTest\Car\LearnTest::addAnswer
+     * @covers TheoryTest\Car\LearnTest::removeAnswer
+     * @covers TheoryTest\Car\LearnTest::replaceAnswer
+     * @covers TheoryTest\Car\LearnTest::sortAnswers
+     * @covers TheoryTest\Car\LearnTest::checkAnswer
+     * @covers TheoryTest\Car\LearnTest::updateLearningProgress
+     * @covers TheoryTest\Car\TheoryTest::getQuestionData
+     * @covers TheoryTest\Car\TheoryTest::getUserID
+     */
+    public function testChangeAnswers()
+    {
+//        $this->learnTest->addAnswer('A', $prim);
+//        $this->learnTest->removeAnswer('A', $prim);
+//        $this->learnTest->replaceAnswer('C', $prim);
+        $this->markTestIncomplete();
+    }
+    
+    /**
+     * @covers TheoryTest\Car\LearnTest::checkAnswer
+     * @covers TheoryTest\Car\LearnTest::updateLearningProgress
+     * @covers TheoryTest\Car\TheoryTest::getUserID
+     */
+    public function testCheckAnswer()
+    {
+//        $this->learnTest->checkAnswer($prim);
+        $this->markTestIncomplete();
+    }
+    
+    /**
+     * @covers TheoryTest\Car\LearnTest::updateLearningProgress
+     * @covers TheoryTest\Car\TheoryTest::getUserID
+     */
+    public function testUpdateLearningProgress()
+    {
+        //$this->learnTest->updateLearningProgress();
         $this->markTestIncomplete();
     }
 }
--- tests/RandomTestTest.php
+++ tests/RandomTestTest.php
@@ -15,7 +15,74 @@ class RandomTestTest extends SetUp
         $this->theoryTest = new RandomTest($this->db, $this->config, $this->template, $this->user);
     }
     
-    public function testConnection()
+    
+    
+    /**
+     * @covers TheoryTest\Car\RandomTest::createNewTest
+     * @covers TheoryTest\Car\RandomTest::chooseQuestions
+     * @covers TheoryTest\Car\RandomTest::setTestName
+     * @covers TheoryTest\Car\TheoryTest::__construct
+     * @covers TheoryTest\Car\TheoryTest::setImagePath
+     * @covers TheoryTest\Car\TheoryTest::clearSettings
+     * @covers TheoryTest\Car\TheoryTest::setTest
+     * @covers TheoryTest\Car\TheoryTest::setTestName
+     * @covers TheoryTest\Car\TheoryTest::anyExisting
+     * @covers TheoryTest\Car\TheoryTest::buildTest
+     * @covers TheoryTest\Car\TheoryTest::getQuestions
+     * @covers TheoryTest\Car\TheoryTest::getUserAnswers
+     * @covers TheoryTest\Car\TheoryTest::getTest
+     * @covers TheoryTest\Car\TheoryTest::checkSettings
+     * @covers TheoryTest\Car\TheoryTest::createQuestionHTML
+     * @covers TheoryTest\Car\TheoryTest::getFirstQuestion
+     * @covers TheoryTest\Car\TheoryTest::getJavascriptLocation
+     * @covers TheoryTest\Car\TheoryTest::getQuestionData
+     * @covers TheoryTest\Car\TheoryTest::getTestName
+     * @covers TheoryTest\Car\TheoryTest::getTestType
+     * @covers TheoryTest\Car\TheoryTest::getUserID
+     * @covers TheoryTest\Car\TheoryTest::numQuestions
+     * @covers TheoryTest\Car\TheoryTest::questionNo
+     * @covers TheoryTest\Car\TheoryTest::updateTestProgress
+     * @covers TheoryTest\Car\TheoryTest::existingScript
+     * @covers TheoryTest\Car\TheoryTest::setImageRootPath
+     * @covers TheoryTest\Car\TheoryTest::setTables
+     * @covers TheoryTest\Car\TheoryTest::addAudio
+     * @covers TheoryTest\Car\TheoryTest::alert
+     * @covers TheoryTest\Car\TheoryTest::audioButton
+     * @covers TheoryTest\Car\TheoryTest::currentQuestion
+     * @covers TheoryTest\Car\TheoryTest::dsaExplanation
+     * @covers TheoryTest\Car\TheoryTest::extraContent
+     * @covers TheoryTest\Car\TheoryTest::flagHintButton
+     * @covers TheoryTest\Car\TheoryTest::getLastQuestion
+     * @covers TheoryTest\Car\TheoryTest::getOptions
+     * @covers TheoryTest\Car\TheoryTest::getScript
+     * @covers TheoryTest\Car\TheoryTest::getUserTestInfo
+     * @covers TheoryTest\Car\TheoryTest::nextQuestion
+     * @covers TheoryTest\Car\TheoryTest::numComplete
+     * @covers TheoryTest\Car\TheoryTest::prevQuestion
+     * @covers TheoryTest\Car\TheoryTest::questionFlagged
+     * @covers TheoryTest\Car\TheoryTest::questionPrim
+     * @covers TheoryTest\Car\TheoryTest::reviewAnswers
+     * @covers TheoryTest\Car\TheoryTest::reviewButton
+     * @covers TheoryTest\Car\TheoryTest::existingLayout
+     * @covers TheoryTest\Car\TheoryTest::getSeconds
+     * @covers TheoryTest\Car\TheoryTest::getTime
+     * @covers TheoryTest\Car\TheoryTest::getUserProgress
+     * @covers TheoryTest\Car\User::checkUserAccess
+     * @covers TheoryTest\Car\User::setUserSettings
+     * @covers TheoryTest\Car\User::getUserSettings
+     */
+    public function testCreateNewTest()
+    {
+         $newTest = $this->theoryTest->createNewTest();
+         $this->assertStringStartsWith('<div class="row">', $newTest);
+        //$this->markTestIncomplete();
+    }
+    
+    /**
+     * @covers TheoryTest\Car\RandomTest::createTestReport
+     * @covers TheoryTest\Car\TheoryTest::createTestReport
+     */
+    public function testCreateReport()
     {
         $this->markTestIncomplete();
     }
